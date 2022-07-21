829e615188cb14e9c8d098293a8c37c5785f50ac
Boolean <lyg2000@aliyun.com>
2022-07-22 00:33:35 +0800

    add

---
+++ stack_rs/target/rls/debug/deps/stack_rs-3d340c8667ab17a2.d
@@ -0,0 +1,5 @@
+f:\RustArithmeticDemo\stack_rs\target\rls\debug\deps\stack_rs-3d340c8667ab17a2.rmeta: src\main.rs
+
+f:\RustArithmeticDemo\stack_rs\target\rls\debug\deps\stack_rs-3d340c8667ab17a2.d: src\main.rs
+
+src\main.rs:
---
+++ stack_rs/target/rls/debug/deps/stack_rs-fb8e7b7353fb5a56.d
@@ -0,0 +1,5 @@
+f:\RustArithmeticDemo\stack_rs\target\rls\debug\deps\stack_rs-fb8e7b7353fb5a56.rmeta: src\main.rs
+
+f:\RustArithmeticDemo\stack_rs\target\rls\debug\deps\stack_rs-fb8e7b7353fb5a56.d: src\main.rs
+
+src\main.rs:
